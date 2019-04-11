7215451a6eede2d9d0b75ac5aa8bcdce6ffd0a1d
taozi <552920369@qq.com>
2019-04-11 18:12:18 +0800

    提交

---
+++ spring-cloud-eureka/src/main/java/com/taozihk/spring/cloud/eureka/EurekaApplication.java
@@ -0,0 +1,21 @@
+package com.taozihk.spring.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 启动类
+ *
+ * @author taozi
+ * @create 2019-04-11 17:47
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+
+}
