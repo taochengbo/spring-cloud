b8f1269f612c5ef3fd0ba724cfca695ca55b62f7
taochengbo <taozihk@163.com>
2019-04-12 22:14:57 +0800

    提交修改

--- spring-cloud-web-admin-feign/src/main/java/com/taozihk/spring/cloud/web/admin/feign/controller/AdminController.java
+++ spring-cloud-web-admin-feign/src/main/java/com/taozihk/spring/cloud/web/admin/feign/controller/AdminController.java
@@ -1,6 +1,5 @@
 package com.taozihk.spring.cloud.web.admin.feign.controller;
 
-
 import com.taozihk.spring.cloud.web.admin.feign.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +7,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * controller类
+ *
+ * @author taozi
+ * @create 2019-04-12 22:06
+ **/
 @RestController
 public class AdminController {
 
     @Autowired
     private AdminService adminService;
 
+    /**
+     * 测试方法
+     * @param message
+     * @return
+     */
     @RequestMapping(value = "hi", method = RequestMethod.GET)
     public String sayHi(@RequestParam String message) {
         return adminService.sayHi(message);
--- spring-cloud-web-admin-feign/src/main/java/com/taozihk/spring/cloud/web/admin/feign/service/AdminService.java
+++ spring-cloud-web-admin-feign/src/main/java/com/taozihk/spring/cloud/web/admin/feign/service/AdminService.java
@@ -1,17 +1,24 @@
 package com.taozihk.spring.cloud.web.admin.feign.service;
 
-
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
- * 通过 @FeignClient("服务名") 注解来指定调用哪个服务。
- */
+ * 通过 @FeignClient("服务名") 注解来指定调用哪个服务
+ *
+ * @author taozi
+ * @create 2019-04-12 22:06
+ **/
 @FeignClient(value = "spring-cloud-service-admin")
 public interface AdminService {
 
+    /**
+     * message方法
+     * @param message
+     * @return
+     */
     @RequestMapping(value = "hi", method = RequestMethod.GET)
     public String sayHi(@RequestParam(value = "message") String message);
 
